98beff38e8cad4b83f4debcfc95cb77512bb494d
Md. Abu Sayeed Mondal <sayeed@Mds-MacBook-Pro.local>
2022-10-18 23:01:12 +0600

    isTestnet file not found error fix

--- nest-unit-integration-test/src/database/database.module.ts
+++ nest-unit-integration-test/src/database/database.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { isTestnet } from 'src/utils/env/is-testnet.utils.env';
+import { isTestnet } from '../utils/env/is-testnet.utils.env';
 isTestnet;
 import { DatabaseService } from './database.service';
 
